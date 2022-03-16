6d1432a1e0b41e4c9d1ef30801bbe63a267cec3a
devJenna <devjennalewis@gmail.com>
2022-03-16 15:29:21 -0700

    update function to add item to the list only when the input value exists

--- app.js
+++ app.js
@@ -9,55 +9,57 @@ newInput.addEventListener("input", function () {
 
 addButton.addEventListener("click", addItems);
 function addItems() {
-    const newItem = newInput.value;
-    console.log(newItem);
+    // add todo only when the input field is not empty
+    if (newInput.value != "") {
+        const newItem = newInput.value;
+        console.log(newItem);
 
-    // change add button back to text
-    addButton.innerHTML = "Add";
+        // change add button back to text
+        addButton.innerHTML = "Add";
 
-    // create li item that contains content div(checkbox and text content) and delete button
-    const newList = document.createElement("li");
-    newList.classList.add("item");
-    listItems.appendChild(newList);
+        // create li item that contains content div(checkbox and text content) and delete button
+        const newList = document.createElement("li");
+        newList.classList.add("item");
+        listItems.appendChild(newList);
 
-    // create content div to include checkbox and text content
-    const newListContent = document.createElement("div");
-    newListContent.classList.add("content");
-    newList.appendChild(newListContent);
+        // create content div to include checkbox and text content
+        const newListContent = document.createElement("div");
+        newListContent.classList.add("content");
+        newList.appendChild(newListContent);
 
-    // add checkbox
-    // const eachCheckbox = document.createElement("span");
-    const eachCheckbox = document.createElement("label");
-    eachCheckbox.classList.add("item-checkbox");
-    eachCheckbox.innerHTML = "<input type='checkbox' class='checkbox'></input>";
-    // newList.appendChild(eachCheckbox);
-    newListContent.appendChild(eachCheckbox);
+        // add checkbox
+        // const eachCheckbox = document.createElement("span");
+        const eachCheckbox = document.createElement("label");
+        eachCheckbox.classList.add("item-checkbox");
+        eachCheckbox.innerHTML = "<input type='checkbox' class='checkbox'></input>";
+        // newList.appendChild(eachCheckbox);
+        newListContent.appendChild(eachCheckbox);
 
-    // add text content
-    const eachContent = document.createElement("div");
-    eachContent.classList.add("item-content");
-    eachContent.innerHTML = newItem;
-    // newList.appendChild(eachContent);
-    newListContent.appendChild(eachContent);
+        // add text content
+        const eachContent = document.createElement("div");
+        eachContent.classList.add("item-content");
+        eachContent.innerHTML = newItem;
+        // newList.appendChild(eachContent);
+        newListContent.appendChild(eachContent);
 
-    // // empty input field
-    // newInput.value = "";
-    // newInput.focus();
+        // // empty input field
+        // newInput.value = "";
+        // newInput.focus();
 
-    // add delete button
-    const deleteButton = document.createElement("span");
-    deleteButton.classList.add("delete");
-    deleteButton.innerHTML = '<i class="icon-cancel-2"></i>';
-    // deleteButton.innerHTML = '<i class="icon-trash"></i>';
-    // listItems.appendChild(deleteButton); 
-    newList.appendChild(deleteButton);
+        // add delete button
+        const deleteButton = document.createElement("span");
+        deleteButton.classList.add("delete");
+        deleteButton.innerHTML = '<i class="icon-cancel-2"></i>';
+        // deleteButton.innerHTML = '<i class="icon-trash"></i>';
+        // listItems.appendChild(deleteButton); 
+        newList.appendChild(deleteButton);
 
 
-    // delete the item
-    deleteButton.addEventListener("click", () => {
-        listItems.removeChild(newList);
-    })
-
+        // delete the item
+        deleteButton.addEventListener("click", () => {
+            listItems.removeChild(newList);
+        })
+    }
     // empty input field
     newInput.value = "";
     newInput.focus();
